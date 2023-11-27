fc9c3b8aaf96658a4b1b2228639a76ef0a8877ab
phatcharee <Patcharee.thabudom30@gmail.com>
2023-11-27 12:30:26 +0700

    Initial commit

---
+++ resources/views/greeting.blade.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+<h1>Hello, {{ $name }} </h1>
+<h1>Hello, <?php echo $name; ?> </h1>
+<h1>Hello, {{ $name }} {{ $last_name }} </h1>
+<h1>Hello, {{ $name . " " . $last_name }} </h1>
+</body>
+</html>
+
+<html>
+<body>
+<h1>Hello, {{ $name }} </h1>
+</body>
+</html>
